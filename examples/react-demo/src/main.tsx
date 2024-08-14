b3018418503381667a831b33f137903a2b672810
CeazzZY <1093918683@qq.com>
2024-08-14 15:14:38 +0800

    feat: init

---
+++ examples/react-demo/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.querySelector('#app')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

d3683a8f10dad9cafb47a66e5052cc7caae1e380
CeazzZY <1093918683@qq.com>
2024-08-23 12:18:51 +0800

    feat: taro-demo init

---
+++ examples/taro-demo/src/pages/index/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from '@tarojs/components';
+import { useLoad } from '@tarojs/taro';
+import './index.css';
+
+export default function Index() {
+  useLoad(() => {
+    console.log('Page loaded.');
+  });
+
+  return (
+    <View className="index">
+      <Text>Hello</Text>
+    </View>
+  );
+}

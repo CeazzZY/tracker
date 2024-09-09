69b33ba13f4ee0756709ea04ca26e8a5679ab10c
CeazzZY <1093918683@qq.com>
2024-09-10 01:07:42 +0800

    feat: wx performance

--- packages/wxapp/src/lib/performance.ts
+++ packages/wxapp/src/lib/performance.ts
@@ -1,12 +1,46 @@
-import { IErr } from '@ceazzzy-tracing/shared';
 import { DataType, ISendData } from 'packages/core/src/types';
 
 export function performanceObserverCallback(
   report: (type: DataType, data: ISendData['data']) => void
 ): PerformanceObserverCallback {
-  return function (list: PerformanceObserverEntryList) {
-    console.log(list.getEntries());
-    const data = {} as IErr;
-    report(DataType.PERFORMANCE, data);
+  return function (entryList: PerformanceObserverEntryList) {
+    for (const entry of entryList.getEntries()) {
+      console.log(entry);
+      let data: any = {};
+
+      switch (entry.entryType) {
+        case 'navigation':
+          data = dealNavigation(entry);
+          break;
+        case 'render':
+          data = dealRender(entry);
+          break;
+        case 'script':
+          data = dealScript(entry);
+          break;
+
+        default:
+          break;
+      }
+      report(DataType.PERFORMANCE, data);
+    }
   };
+}
+
+function dealNavigation(entry: PerformanceEntry) {
+  const { name, duration, path, startTime } = entry as any;
+
+  return { name, duration, path, startTime };
+}
+
+function dealRender(entry: PerformanceEntry) {
+  const { name, duration, path, startTime } = entry as any;
+
+  return { name, duration, path, startTime };
+}
+
+function dealScript(entry: PerformanceEntry) {
+  const { name, duration, fileList, startTime } = entry as any;
+
+  return { name, duration, fileList, startTime };
 }

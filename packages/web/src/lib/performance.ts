75cf60d4dc6eec68a3d4e6c711ec99eee368c803
CeazzZY <1093918683@qq.com>
2024-09-09 16:12:46 +0800

    feat: navigationTiming

--- packages/web/src/lib/performance.ts
+++ packages/web/src/lib/performance.ts
@@ -1,41 +1,81 @@
-import { IErr } from '@ceazzzy-tracing/shared';
+import { AnyObj, IErr } from '@ceazzzy-tracing/shared';
 import { DataType, ISendData } from 'packages/core/src/types';
 
 export function performanceObserverCallback(
   report: (type: DataType, data: ISendData['data']) => void
 ): PerformanceObserverCallback {
-  return function (list: PerformanceObserverEntryList) {
-    console.log(list.getEntries());
+  return function (entryList: PerformanceObserverEntryList) {
+    for (const list of entryList.getEntries()) {
+      switch (list.entryType) {
+        case 'navigation':
+          dealNavigationTime(list as PerformanceNavigationTiming);
+          break;
+        case 'resource':
+          dealResource(list as PerformanceResourceTiming);
+          break;
+
+        default:
+          console.warn('获取未知性能 entryType');
+          break;
+      }
+    }
     const data = {} as IErr;
     report(DataType.PERFORMANCE, data);
   };
-  //   const t: any = performance.getEntriesByType('navigation')[0];
+}
 
-  //   times.tti = t.domInteractive - t.fetchStart; // 首次可交互时间
+function dealNavigationTime(navigation: PerformanceNavigationTiming) {
+  const {
+    domainLookupStart,
+    domainLookupEnd,
+    connectStart,
+    connectEnd,
+    secureConnectionStart,
+    requestStart,
+    responseStart,
+    responseEnd,
+    domInteractive,
+    domContentLoadedEventEnd,
+    loadEventStart,
+    fetchStart,
+    redirectEnd,
+    redirectStart,
+    unloadEventEnd,
+    unloadEventStart,
+  } = navigation;
+  const times: AnyObj = {};
 
-  //   times.ready = t.domContentLoadedEventEnd - t.fetchStart; // HTML加载完成时间
+  times.tti = domInteractive - fetchStart; // 首次可交互时间
 
-  //   times.loadon = t.loadEventStart - t.fetchStart; // 页面完全加载时间
+  times.ready = domContentLoadedEventEnd - fetchStart; // HTML加载完成时间
 
-  //   times.firstbyte = t.responseStart - t.domainLookupStart; // 首包时间
+  times.loadon = loadEventStart - fetchStart; // 页面完全加载时间
 
-  //   times.dns = t.domainLookupEnd - t.domainLookupStart; // dns查询耗时
+  times.firstbyte = responseStart - domainLookupStart; // 首包时间
 
-  //   times.appcache = t.domainLookupStart - t.fetchStart; // dns缓存时间
+  times.dns = domainLookupEnd - domainLookupStart; // dns查询耗时
 
-  //   times.tcp = t.connectEnd - t.connectStart; // tcp连接耗时
+  times.appcache = domainLookupStart - fetchStart; // dns缓存时间
 
-  //   times.ttfb = t.responseStart - t.requestStart; // 请求响应耗时
+  times.tcp = connectEnd - connectStart; // tcp连接耗时
 
-  //   times.trans = t.responseEnd - t.responseStart; // 内容传输耗时
+  times.ttfb = responseStart - requestStart; // 请求响应耗时
 
-  //   times.dom = t.domInteractive - t.responseEnd; // dom解析耗时
+  times.trans = responseEnd - responseStart; // 内容传输耗时
 
-  //   times.res = t.loadEventStart - t.domContentLoadedEventEnd; // 同步资源加载耗时
+  times.dom = domInteractive - responseEnd; // dom解析耗时
 
-  //   times.ssllink = t.connectEnd - t.secureConnectionStart; // SSL安全连接耗时
+  times.res = loadEventStart - domContentLoadedEventEnd; // 同步资源加载耗时
 
-  //   times.redirect = t.redirectEnd - t.redirectStart; // 重定向时间
+  times.ssllink = connectEnd - secureConnectionStart; // SSL安全连接耗时
 
-  //   times.unloadTime = t.unloadEventEnd - t.unloadEventStart; // 上一个页面的卸载耗时
+  times.redirect = redirectEnd - redirectStart; // 重定向时间
+
+  times.unloadTime = unloadEventEnd - unloadEventStart; // 上一个页面的卸载耗时
+
+  console.log(times);
+}
+
+function dealResource(resource: PerformanceResourceTiming) {
+  console.log(resource);
 }
